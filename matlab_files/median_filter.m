e24e5b6451eb27914378f6e40820afe001317c3d
Lucas Santos <36669245+lucascorreiabs@users.noreply.github.com>
2018-09-04 21:22:32 -0300

    MATLAB Files

---
+++ matlab_files/median_filter.m
@@ -0,0 +1,11 @@
+% Aplicação do Filtro da Mediana
+
+img = imread('CNN1.png');
+img = rgb2gray(img); %transforma a imagem em niveis de cinza
+
+img_noise = imnoise(img, 'salt & pepper', 0.02); %adiciona ruido salt and pepper a imagem
+ 
+med_filt = medfilt2(img_noise, [3 3]); %aplica o filtro da mediana 3x3 na imagem
+
+%imshow(img_noise); %exibe a imagem com ruidos
+imshow(med_filt); % exibe o resultado
